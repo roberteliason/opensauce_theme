8c3f091f259eda85b77c27bce2f2a6715610d1a2
Robert Eliason <roberte@orestad-linux.se>
2015-08-30 19:12:32 +0200

    Increasing cache TTL on recipe page

--- content-recipe.php
+++ content-recipe.php
@@ -7,25 +7,25 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>  itemscope itemtype="http://schema.org/Recipe">
     <div class="intro">
 
-	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_image_slider' , 'opensauce_render_recipe_image_slider', 30 ); ?>
+	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_image_slider' , 'opensauce_render_recipe_image_slider', HOUR_IN_SECONDS ); ?>
 
         <header>
             <?php the_title( '<h1 class="entry-title" itemprop="name">', '</h1>' ); ?>
         </header>
         <div class="columns">
 
-	        <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_body' , 'opensauce_render_recipe_body', 30 ); ?>
-	        <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_meta' , 'opensauce_render_recipe_meta', 30 ); ?>
+	        <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_body' , 'opensauce_render_recipe_body', HOUR_IN_SECONDS ); ?>
+	        <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_meta' , 'opensauce_render_recipe_meta', HOUR_IN_SECONDS ); ?>
 
         </div>
     </div>
 
     <div class="col-container">
-	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_ingredients' , 'opensauce_render_recipe_ingredients', 30 ); ?>
-	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_steps' , 'opensauce_render_recipe_steps', 30 ); ?>
+	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_ingredients' , 'opensauce_render_recipe_ingredients', HOUR_IN_SECONDS ); ?>
+	    <?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_steps' , 'opensauce_render_recipe_steps', HOUR_IN_SECONDS ); ?>
     </div>
 
-	<?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_nonsense' , 'opensauce_render_recipe_nonsense', 30 ); ?>
-	<?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_social' , 'opensauce_render_recipe_social', 30 ); ?>
+	<?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_nonsense' , 'opensauce_render_recipe_nonsense', HOUR_IN_SECONDS ); ?>
+	<?php opensauce_maybe_cache_output( 'recipe_'.get_the_ID().'_social' , 'opensauce_render_recipe_social', HOUR_IN_SECONDS ); ?>
 
 </article>
